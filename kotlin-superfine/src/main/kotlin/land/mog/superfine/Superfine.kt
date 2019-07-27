78d68621c57105cef9fd29853fb8accadfd10146
monchi <poyo0315@gmail.com>
2019-07-28 03:50:30 +0900

    Apply suitable interface to VNode

--- kotlin-superfine/src/main/kotlin/land/mog/superfine/Superfine.kt
+++ kotlin-superfine/src/main/kotlin/land/mog/superfine/Superfine.kt
@@ -1,3 +1,12 @@
 package land.mog.superfine
 
-interface VNode
+import org.w3c.dom.Node
+
+external interface VNode {
+    val name: String
+    val props: dynamic
+    val children: List<VNode>
+    val node: Node?
+    val type: Short?
+    val key: String?
+}
